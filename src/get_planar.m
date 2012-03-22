3c58e2c20f0bbbee4c7eca47f7cd514f702228de
Sean Wilson <s0831408@moose.inf.ed.ac.uk>
2012-03-22 12:59:06 +0000

    Started planar detection code

---
+++ src/get_planar.m
@@ -0,0 +1,23 @@
+function planar = get_planar(pixel_list, threshold)
+
+%This function will take a list form of image (NumPixels, 6) and return
+%the corner pixels of the planar [TL, BL, BR, TR] (y,x)
+%Threshold is thresh level of RGB (R,G,B)
+
+%% Find the pixels that are not suitable and remove
+for i = 1 : size(pixel_list,1)
+	if (pixel_list(i,4) > threshold(1) && pixel_list(i,5) > threshold(2) && pixel_list(i,6) > threshold(3))
+		pixel_list(i, :) = [];
+	end
+end	
+
+%% Randomly select point and grow, continue until grows no more or too large
+
+
+%% Fit plane to the patch so far, if error to large, not planar
+
+
+%% Use RANSAC to get lines
+
+
+%% Get intersections of lines and return as planar corners
