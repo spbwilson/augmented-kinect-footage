21d539ee499f59c43540428a35b2e23fb068de8d
Stephen McGruer <stephen_mcgruer@gmail.com>
2012-04-04 15:35:36 +0100

    Added the other briefcase frames (apart from 12), manually calculated.

--- src/get_briefcase_coords.m
+++ src/get_briefcase_coords.m
@@ -4,7 +4,19 @@ function [ UVs, XY ] = get_briefcase_coords(frames, debug)
 
 UVs = cell(36, 1);
 
-% We have known data for 14 to 28.
+% There is no briefcase in 1-8.
+
+% 9-11 have been determined manually.
+UVs{9}  = [[310, -49]', [408, -32]', [385, 81]', [287, 64]']';
+UVs{10} = [[321, -57]', [414, -58]', [415, 57]', [322, 58]']';
+UVs{11} = [[324, -64]', [407, -81]', [430, 32]', [347, 49]']';
+
+% 12 does not have enough data.
+
+% 13 has been determined manually.
+UVs{13} = [[297, -29]', [401, -66]', [440, 42]', [336, 79]']';
+
+% We have automatic computation for 14 to 28.
 for i = 14 : 28
     image = permute(reshape(frames{i}, [640 480 6]), [2 1 3]);
 
@@ -12,6 +24,9 @@ for i = 14 : 28
 
     UVs{i} = [TL([2 1]), BL([2 1]), BR([2 1]), TR([2 1])]';
 end
+
+% 29 has been determined manually.
+UVs{29} = [[314, 550]', [408, 546]', [399, 661]', [305, 665]']';
 
 XY = [[1, 1]', [480, 1]', [480, 640]', [1, 640]']';
 
--- src/show_briefcase.m
+++ src/show_briefcase.m
@@ -2,6 +2,9 @@ function [ image ] = show_briefcase( image, UV, XY, source_image )
 %SHOW_BRIEFBASE Summary of this function goes here
 %   Detailed explanation goes here
 
+image_height = size(image, 1);
+image_width  = size(image, 2);
+
 maxs = max(UV);
 mins = min(UV);
 
@@ -16,6 +19,10 @@ min_y = mins(1);
 
 for r = min_y : max_y
     for c = min_x : max_x
+        if (r < 1 || c < 18 || r > image_height || c > 604)
+            continue
+        end
+
         r_i = r - (min_y - 1);
         c_i = c - (min_x - 1);
         if sum(homo_image(r_i, c_i, :)) > 0
